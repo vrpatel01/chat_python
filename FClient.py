8f2656ec23535a02e93ae48aa1aa253f63c8a752
Vipul Patel <54105991+vrpatel01@users.noreply.github.com>
2021-05-25 12:00:09 +0530

    Add files via upload

---
+++ FClient.py
@@ -0,0 +1,333 @@
+import socket
+import threading
+import pickle
+from getpass import getpass
+from datetime import datetime
+
+class PACKET:
+    def __init__(self,TYPE,sender,recever='SERVER',data=None):
+        self.time = datetime.now()
+        self.TYPE = TYPE
+        self.sender = sender
+        self.recever = recever
+        self.data = data
+
+
+class CLIENT():
+
+    def __init__(self):
+        print('WELCOME')
+        # (input('Enter SERVER IP: '),input('Enrer SERVER port: '))
+        self.S_addr = ("127.0.0.1", 45000)
+        self.HName = socket.gethostname()
+        self.ip = socket.gethostbyname(self.HName)
+        self.Groupes = {}
+        self.mes_history = []
+        self.new_mes = []
+
+        self.c_rec = self.connect_server()
+        if not self.load_reg():
+            print('WELCOME\n1. New ACCOUNT\n2. Login')
+            ch = input('>> ')
+            if ch == '1':
+                self.regester()
+                self.save_reg()
+                self.login(True)
+            elif ch == '2':
+                self.login(False)
+        else:
+            self.login(True)
+            self.request('REGESTRATION_DETAIL')
+
+        thread = threading.Thread(target=self.lisen_forever)
+        thread.start()
+
+        print("1. SEND \t2. SHOW\t3. LOAD OLD\t4. LOAD NEW\t5. New Group")
+        while True:
+            ch = input('>')
+            if ch == '1':
+                self.send_msg()
+            elif ch == '2':
+                self.show()
+            elif ch == '3':
+                self.request('OLD_MESSAGES')
+            elif ch == '4':
+                self.request('NEW_MESSAGES')
+            elif ch == '5':
+                self.create_group()
+            else:
+                pp = input("EXIT 0 : ")
+                if pp == "0":
+                    self.send_stop()
+                    break
+        else:
+            print('SOMTHINGs WRONG ')
+
+    def create_group(self):
+        members = []
+        print('Enter Group info')
+        name = input('Enter Name of group (unique)')
+        while True:
+            try:
+                no_membes = int(input('number of members: '))
+                break
+            except Exception as e:
+                print(e)
+        for i in range(no_membes):
+            memb = input('Menber nick name > ')
+            members.append(memb)
+        obj = PACKET('C_GROUP',self.nickName)
+        obj.uname = self.uname
+        obj.members = members
+        obj.group_name = name
+        self.send_packet(self.c_snd,obj)
+
+    def lisen_forever(self):
+        conn = self.c_rec
+        while True:
+            obj = self.rec_packet(conn)
+            if obj.TYPE == 'I_MESSAGE':
+                self.mes_history.append(obj)
+                print(obj.data)
+            elif obj.TYPE == 'G_MESSAGE':
+                self.mes_history.append(obj)
+                print(obj.data)
+            elif obj.TYPE == 'C_GROUP':
+                if obj.ack:
+                    print(obj.message)
+                else:
+                    print(obj.message)
+                    l = input('RETRY 1 >')
+                    if l == '1':
+                        self.create_group()
+            elif obj.TYPE == 'OLD_MESSAGES':
+                self.load_mesgs(obj.data)
+            elif obj.TYPE == 'NEW_MESSAGES':
+                if obj.ack:
+                    self.load_new_mesgs(obj.data)
+                else:
+                    print('No new messages')
+            elif obj.TYPE == 'REGESTRATION_DETAIL':
+                self.load_reg(obj.data)
+            elif obj.TYPE == 'GROUP_ADDED':
+                self.mes_history.append(obj)
+                print(obj.message)
+            elif obj.TYPE == 'STOP':
+                self.send_stop()
+            else:
+                print(f'INVALID OBJECT {obj.TYPE},{obj.data}')
+
+    def create_reg(self):
+        print('Enter the information to create account:\t')
+        print('[username] (not shared with anyone alphanumeric)')
+        self.uname = input('>')
+        print('[password] (alphanumeric with symbols)')
+        self.passwd = input('>')
+        print('[Nick Name] what people will see you as alphanumeric ')
+        self.nickName = input('>')
+        self.snid = "SNID"
+        self.HName = socket.gethostname()
+        self.IP = socket.gethostbyname(self.HName)
+        self.PubKey = "PKY"
+        self.PrvKey = "KEY"
+
+        print('Cheacking for validation ...')
+
+    def regester(self):
+        self.create_reg()
+        if self.register_():
+            return True
+        else:
+            return self.regester()
+
+    def register_(self):
+        conn = self.connect_server()
+        dct = {'uname':self.uname,'passwd':self.passwd,'nickName':self.nickName,'ip':self.ip,'HName':self.HName,'PubKey':self.PubKey}
+        obj = PACKET('REGISTER', self.uname, 'SERVER', dct)
+        obj.uname = self.uname
+        obj.nickName = self.nickName
+        obj.passwd = self.passwd
+        self.send_packet(conn,obj)
+        obj = self.rec_packet(conn)
+        conn.close()
+        if obj.ack:
+            print(f'Regestration Successful. uname: {obj.uname} passwd: {obj.passwd} nickName:{obj.nickName}')
+            self.uname = obj.uname
+            self.passwd = obj.passwd
+            return True
+        else:
+            print(
+                f"Regestration Unseccessful. uname: {obj.uname} passwd: {obj.passwd}  nickName:{obj.nickName}")
+            return False
+
+    def save_reg(self):
+        with open('reg', 'wb') as f:
+            pickle.dump([self.Groupes,self.uname,self.passwd,self.nickName, self.PubKey, self.PrvKey], f)
+
+    def load_reg(self,data=False):
+        if not data:
+            try:
+                with open('reg', 'rb') as f:
+                    self.Groupes, self.uname, self.passwd, self.nickName, self.PubKey, self.PrvKey = pickle.load(
+                        f)
+                    self.load_mesgs()
+                    return True
+            except Exception as e:
+                print(e)
+            else:
+                return False
+        else:
+            self.Groupes, self.uname, self.passwd, self.nickName, self.PubKey, self.PrvKey = data
+            print('data got from server', self.Groupes, self.uname,
+                  self.passwd, self.nickName, self.PubKey, self.PrvKey)
+
+    def send_packet(self,conn,obj):
+        print(obj.uname)
+        packet = pickle.dumps(obj)
+        l = str(len(packet)).rjust(10,' ').encode('utf-8')
+        conn.send(l)
+        conn.send(packet)
+
+    def rec_packet(self,conn):
+        while True:
+            l = conn.recv(10).decode('utf-8')
+            if l:
+                l = int(l)
+                while True:
+                    packet = conn.recv(l)
+                    if packet:
+                        obj = pickle.loads(packet)
+                        return obj
+
+    def login(self,saved=False):
+        if saved:
+            uname = self.uname
+            passwd = self.passwd
+        else:
+            uname = input("ENTER USERNAME \t>")
+            passwd = getpass('ENTER PASSWORD \t>')
+
+        if self.login_(uname,passwd):
+            if self.login_rec():
+                return True
+        else:
+            return self.login()
+
+    def login_rec(self):
+        conn = self.connect_server()
+        obj = PACKET('LOGIN_REC',self.uname)
+        obj.snid_rec = self.snid_rec
+        obj.uname = self.uname
+        self.send_packet(conn, obj)
+        obj = self.rec_packet(conn)
+        if obj.ack:
+            self.c_rec = conn
+            return True
+        else:
+            print('SOMTHING IS VERY WRONG\r restart the program and register  again...')
+            conn.close()
+            exit()
+            return False
+
+    def login_(self,uname,passwd):
+        conn = self.connect_server()
+        obj = PACKET('LOGIN',uname,'SERVER')
+        obj.uname = uname
+        obj.passwd = passwd
+        self.send_packet(conn, obj)
+        obj = self.rec_packet(conn)
+        if obj.ack:
+            print(f'Login Successful. nickname: {obj.nickName}, snid_rec {obj.snid_rec}')
+            self.uname = uname
+            self.passwd = passwd
+            self.nickName = obj.nickName
+            self.snid_rec = obj.snid_rec
+            self.c_snd = conn
+            return True
+        else:
+            print(
+                f"Login Unseccessful. {obj.messages}")
+            conn.close()
+            return False
+
+    def send_msg(self):
+        ch = input('1. Indevidual\t2. Group')
+        if ch == '1':
+            to_nickName = input("Send to: ")
+            data = input("mes: ")
+            obj = PACKET('I_MESSAGE', self.nickName, to_nickName, data)
+        else:
+            to = input("Send to: ")
+            data = input("mes: ")
+            obj = PACKET('G_MESSAGE', self.nickName, to, data)
+        obj.uname = self.uname
+        obj.nickName = self.nickName
+        self.send_packet(self.c_snd, obj)
+
+    def send_stop(self):
+        self.save_mesgs()
+        try:
+            obj = PACKET('STOP',self.uname,'SERVER')
+            obj.uname = self.uname
+            self.send_packet(self.c_snd,obj)
+            self.c_snd.close()
+            print(f'[connection closed] sender')
+        except Exception as e:
+            print(e)
+        exit()
+
+    def load_mesgs(self,data=False):
+        if not data:
+            try:
+                with open('messages', 'rb') as f:
+                    self.mes_history = pickle.load(f)
+                    return True
+            except Exception as e:
+                print(e)
+            else:
+                return False
+        else:
+            self.mes_history = data
+
+    def load_new_mesgs(self,data):
+        print('daata of neww messages',data)
+        self.new_mes = data
+        print(f'{len(self.new_mes)} new mwssages..')
+        self.show(self.new_mes)
+        self.mes_history.append(self.new_mes)
+        self.new_mes=[]
+
+    def save_mesgs(self):
+        with open('messages', 'wb') as f:
+            pickle.dump(self.mes_history, f)
+        
+    def request(self,TYPE):
+        print('requesting details')
+        obj = PACKET(TYPE,self.uname,'SERVER')
+        obj.uname = self.uname
+        obj.nickName = self.nickName
+        self.send_packet(self.c_snd,obj)
+
+    def show(self,ls=False):
+        lst = ls if ls else self.mes_history
+        senders = []
+        for mes in lst:
+            senders.append(mes.sender)
+        senders = set(senders)
+        print(senders)
+        for mes in lst:
+            sender = mes.sender
+            s= sender.ljust(10," ")
+            s+='> ' + mes.data
+            s = s.ljust(64, ' ') + '\t' + mes.time.strftime("%H:%M:%S")
+            print(s)
+
+    def connect_server(self, addrs=False):
+        if not addrs:
+            addrs = self.S_addr
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(addrs)
+        return client
+
+if __name__ == '__main__':
+    me = CLIENT()
---
+++ sock-1.py
@@ -0,0 +1,327 @@
+import socket
+import threading
+import pickle
+from datetime import datetime
+
+
+class PACKET:
+    def __init__(self, TYPE, sender='SERVER', recever='ME', data=None):
+        self.time = datetime.now()
+        self.TYPE = TYPE
+        self.sender = sender
+        self.recever = recever
+        self.data = data
+
+
+class Client_():
+    def __init__(self, dct):
+        self.uname = dct['uname']
+        self.passwd = dct['passwd']
+        self.nickName = dct['nickName']
+        self.HName = dct['HName']
+        self.ip = dct['ip']
+        self.PubKey = dct['PubKey']
+        self.PrvKey = 'no'
+        self.Groupes = []
+        self.Blocked = []
+        self.old_mesgs = []
+        self.new_mes = []
+
+    def reg_det(self):
+        return[self.Groupes, self.uname, self.passwd, self.nickName, self.PubKey, self.PrvKey]
+
+
+class Group_():
+    def __init__(self, name, admin, ls):
+        self.name = name
+        self.admin = admin
+        self.members = [admin, *ls]
+
+    def members_ls(self, uname):
+        temp = self.members
+        temp.remove(uname)
+        return temp
+
+
+class SERVER():
+
+    def __init__(self, addrs=('127.0.0.1', 45000)):
+        self.S_addrs = addrs
+        self.Recevers = {}
+        self.Senders = {}
+        self.nickNames = {}
+        self.online = []
+        self.snid_ls = ['A23Sc22', 'Acce35a',
+                        'Nj3HG3', '87Htr5D', 'JHG76b', 'jtF5rs']
+        if not self.load_data():
+            self.clients = {}
+            self.nickNames = {}
+            self.groupes = {}
+
+    def start(self, S_addrs=False):
+        if not S_addrs:
+            S_addrs = self.S_addrs
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(addrs)
+        server.listen()
+        while True:
+            conn, addr = server.accept()
+            thread = threading.Thread(
+                target=self.handle_client, args=(conn,))
+            thread.start()
+
+    def handle_client(self, conn):
+        while True:
+            obj = self.rec_packet(conn)
+            if obj.TYPE == 'REGISTER':
+                self.register_client(conn, obj)
+            elif obj.TYPE == 'LOGIN':
+                self.login_client(conn, obj)
+            elif obj.TYPE == 'LOGIN_REC':
+                self.login_client_rec(conn, obj)
+            return
+
+    def register_client(self, conn, obj):
+        print(f'Regestering {obj.uname}, {obj.data}')
+        obj = self.check_client(obj)
+        if obj.ack:
+            self.register(conn, obj)
+            print(f'Regestered {obj.uname}')
+        else:
+            self.send_packet(conn, obj)
+            print(f'regesteration failed {obj.uname}')
+        self.send_packet(conn, obj)
+
+    def login_client_rec(self, conn, obj):
+        if obj.snid_rec == self.clients[obj.uname].snid_rec:
+            obj.ack = True
+            self.Recevers[obj.uname] = conn
+        else:
+            obj.ack = False
+        self.send_packet(conn, obj)
+        if obj.ack:
+            conn = self.Senders[obj.uname]
+            self.online.append(obj.uname)
+            self.lisen_forever(conn)
+
+    def login_client(self, conn, obj):
+        if obj.uname in self.clients:
+            client = self.clients[obj.uname]
+            if obj.passwd == client.passwd:
+                print(f'{obj.uname} Loged in')
+                snid_rec = self.allocate_snid()
+                self.clients[obj.uname].snid_rec = snid_rec
+                obj.snid_rec = snid_rec
+                obj.nickName = client.nickName
+                obj.ack = True
+        else:
+            obj.ack = False
+            obj.messages = 'INVALID_USERNAME_PASSWORD'
+        self.send_packet(conn, obj)
+        if obj.ack:
+            self.Senders[obj.uname] = conn
+
+    def lisen_forever(self, conn):
+        while True:
+            obj = self.rec_packet(conn)
+            if obj.TYPE == 'I_MESSAGE':
+                self.froward_message(obj)
+            elif obj.TYPE == 'G_MESSAGE':
+                print('Groip mess here')
+                self.froward_message_group(obj)
+            elif obj.TYPE == 'C_GROUP':
+                self.create_group(obj)
+            elif obj.TYPE == 'OLD_MESSAGES' or obj.TYPE == 'NEW_MESSAGES' or obj.TYPE == 'REGESTRATION_DETAIL':
+                self.send_data(obj)
+            elif obj.TYPE == 'STOP':
+                self.stop_client(obj)
+                return
+            else:
+                print(f'INVALID OBJECT {obj.TYPE},{obj.data}')
+
+    def stop_client(self, obj):
+        conn_r = self.Recevers[obj.uname]
+        conn_s = self.Senders[obj.uname]
+        self.send_packet(conn_r, PACKET(TYPE='STOP'))
+        self.close_conn(conn_r)
+        self.close_conn(conn_s)
+        del self.Recevers[obj.uname]
+        del self.Senders[obj.uname]
+        self.online.remove(obj.uname)
+        print(f'{obj.uname} loged out')
+        print('online clients', self.online)
+
+    def close_conn(self, conn):
+        try:
+            conn.close()
+        except Exception as e:
+            print(e)
+
+    def send_data(self, obj):
+        client = self.clients[obj.uname]
+        if obj.TYPE == 'OLD_MESSAGES':
+            data = client.old_mesgs
+        elif obj.TYPE == 'NEW_MESSAGES':
+            if len(client.new_mes) > 0:
+                data = client.new_mes
+                client.old_mesgs.append(data)
+                client.new_mes = []
+            else:
+                data = 'FALSE'
+        elif obj.TYPE == 'REGESTRATION_DETAIL':
+            data = client.reg_det()
+        obj.data = data
+        obj.ack = False if data == 'FALSE' else  True
+
+        self.send_packet(self.Recevers[obj.uname], obj)
+
+    def create_group(self, obj):
+        if obj.group_name not in self.groupes:
+            nickNames = obj.members
+            unames = []
+            valid_nickNames = []
+            invalid_nicknames = []
+            b_unames = []
+            for nickName in nickNames:
+                if nickName in self.nickNames:
+                    client = self.nickNames[nickName]
+                    if obj.uname not in client.Blocked:
+                        unames.append(client.uname)
+                        valid_nickNames.append(client.nickName)
+                    else:
+                        b_unames.append(nickName)
+                else:
+                    invalid_nicknames.append(nickName)
+            if len(unames) > 0:
+                obj.ack = True
+                obj.message = 'Group Created '
+                obj.nickNames = valid_nickNames
+                obj.invalid_nicknames = invalid_nicknames
+                obj.blocked = b_unames
+                group = Group_(obj.group_name, obj.uname, unames)
+                print('Group created with ', group.name,
+                      group.admin, group.members,)
+                self.save_data()
+                self.updata_member(group)
+            else:
+                obj.ack = False
+                obj.message = 'Goup Not Created'
+                obj.invalid_nicknames = invalid_nicknames
+                obj.blocked = b_unames
+        else:
+            obj.ack = False
+            obj.message = 'Invalid Group Name'
+        self.send_packet(self.Recevers[obj.uname], obj)
+
+    def updata_member(self, group):
+        unames = group.members
+        obj = PACKET('GROUP_ADDED', group.admin)
+        obj.uname = group.admin
+        obj.group_name = group.name
+        obj.message = f'YOU WERE ADDED IN GROUP {group.name} BY {self.nickNames[group.admin].nickName}'
+        for uname in unames:
+            self.clients[uname].Groupes.append(group.name)
+            obj.recever = self.nickNames[uname].nickName
+            self.froward_message(obj)
+
+    def froward_message_group(self, obj):
+        if obj.recever in self.groupes:
+            group = self.groupes
+            recevers = group.members_ls(obj.uname)
+            print('recevers list here ',recevers)
+            for recever in recevers:
+                obj.recever = self.nickNames[recever].nickName
+                self.froward_message(obj)
+
+    def froward_message(self, obj):
+        if not obj.data:
+            obj.data = ''
+        if obj.recever in self.nickNames:
+            recever = self.nickNames[obj.recever]
+            sender = self.clients[obj.uname]
+            if recever.uname in self.online:
+                self.send_packet(self.Recevers[recever.uname], obj)
+                recever.old_mesgs.append(obj)
+            else:
+                recever.new_mes.append(obj)
+            sender.old_mesgs.append(obj)
+            obj.ack = True
+        else:
+            obj.ack = False
+            obj.messages = 'INVALID_NICK_NAME'
+            print(f'Invalid nickname use by {obj.sender} : {obj.recever}')
+        return obj
+
+    def register(self, conn, obj):
+        dct = obj.data
+        c = Client_(obj.data)
+        self.clients[obj.uname] = c
+        self.nickNames[obj.nickName] = c
+        self.save_data()
+
+    def check_client(self, obj):
+        if obj.uname not in self.clients:
+            if obj.nickName not in self.nickNames:
+                if self.check_passwd(obj.passwd):
+                    obj.ack = True
+                    return obj
+                else:
+                    obj.passwd = 'WEAK_PASSWORD'
+            else:
+                obj.nickName = 'INVALID_NICK_NAME'
+        else:
+            obj.uname = 'INVALID_USER_NAME'
+        obj.ack = False
+        return obj
+
+    def check_passwd(self, passwd):
+        return True
+
+    def send_packet(self, conn, obj):
+        try:
+            packet = pickle.dumps(obj)
+            l = str(len(packet)).rjust(10, ' ').encode('utf-8')
+            conn.send(l)
+            conn.send(packet)
+        except Exception as e:
+            print(e)
+
+    def rec_packet(self, conn):
+        while True:
+            l = conn.recv(10).decode('utf-8')
+            if l:
+                l = int(l)
+                while True:
+                    packet = conn.recv(l)
+                    if packet:
+                        obj = pickle.loads(packet)
+                        return obj
+
+    def manage_server(self):
+        zzz = input('>.')
+        if zzz == '0':
+            self.save_data()
+            exit()
+
+    def save_data(self):
+        with open('clients_info', 'wb') as f:
+            pickle.dump([self.clients, self.nickNames, self.groupes], f)
+
+    def load_data(self):
+        try:
+            with open('clients_info', 'rb') as f:
+                self.clients, self.nickNames,  self.groupes = pickle.load(f)
+                return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def allocate_snid(self):
+        return self.snid_ls[0]
+
+
+if __name__ == '__main__':
+    addrs = ('127.0.0.1', 45000)
+    server1 = SERVER(addrs)
+    server1.start()
+    server1.save_clients()
